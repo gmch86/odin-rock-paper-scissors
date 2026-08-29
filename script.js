a898dc6375c0b75cd37628c37ac818f79cf2afef
Grant <76251920+gmch86@users.noreply.github.com>
2026-08-29 20:18:59 +0100

    feat(script.js): Added base structure game

---
+++ script.js
@@ -0,0 +1,21 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  // returns string values "rock", "paper" or "scissors"
+}
+
+function getHumanChoice() {
+  // prompts user for a choice between "rock", "paper" or "scissors"
+}
+
+function playRound(humanChoice, computerChoice) {
+  // make humanChoice case-insensitive
+  // increments score of round winner
+  // logs round winner to console
+}
+
+function playGame() {
+  // keeps track of scores
+  // calls playRound 5 times before declaring winner
+}
