85454186512ffeb5ff83426dbda51cdde96d60ac
Grant <76251920+gmch86@users.noreply.github.com>
2026-09-02 18:49:03 +0100

    refactor(index.html, script.js): Grouped logic into a main function for better organization and readability.

--- script.js
+++ script.js
@@ -1,185 +1,187 @@
-let humanScore = 0;
-let computerScore = 0;
+main();
 
-const uiCheckbox = document.querySelector("#ui");
-const playBtn = document.querySelector("button.play-btn");
-const rockBtn = document.querySelector("button.rock-btn");
-const paperBtn = document.querySelector("button.paper-btn");
-const scissorsBtn = document.querySelector("button.scissors-btn");
+function main() {
+  const uiCheckbox = document.querySelector("#ui");
+  const playBtn = document.querySelector("button.play-btn");
+  const rockBtn = document.querySelector("button.rock-btn");
+  const paperBtn = document.querySelector("button.paper-btn");
+  const scissorsBtn = document.querySelector("button.scissors-btn");
 
-let uiEnabled = uiCheckbox.checked;
-
-uiCheckbox.addEventListener("change", (e) => {
-  uiEnabled = e.target.checked;
-});
-
-playBtn.addEventListener("click", (e) => {
-  playGame();
-});
-
-function getComputerChoice() {
-  // Random number between 1-3
-  const choice = Math.floor(Math.random() * 3) + 1;
-
-  switch (choice) {
-    case 1:
-      return "rock";
-    case 2:
-      return "paper";
-    case 3:
-      return "scissors";
-  }
-}
-
-function getHumanChoice() {
-  // Prompts user for a choice and convert to all lowercase
-  let choice = window.prompt(`Choose "rock", "paper" or "scissors"!`);
-
-  if (choice) {
-    choice = choice.toLowerCase();
-  }
-
-  // Validate choice
-  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-    const error = new Error(`Choice must be "rock", "paper" or "scissors"`, {
-      cause: `Invalid choice. Received: "${choice}`,
-    });
-
-    error.value = choice;
-    throw error;
-  } else {
-    return choice;
-  }
-}
-
-function playRound(humanChoice, computerChoice) {
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  const rules = {
-    rock: { winsAgainst: "scissors", losesAgainst: "paper" },
-    paper: { winsAgainst: "rock", losesAgainst: "scissors" },
-    scissors: { winsAgainst: "paper", losesAgainst: "rock" },
+  // Object to store game data
+  const game = {
+    humanScore: 1,
+    computerScore: 1,
+    numberOfRounds: 5,
+    currentRound: null,
+    result: null,
   };
 
-  // Determine winner and increase scores
-  let msg;
+  let uiEnabled = uiCheckbox.checked;
 
-  if (humanChoice === computerChoice) {
-    msg = "It's a draw!";
-  } else if (rules[humanChoice].winsAgainst === computerChoice) {
-    msg = `You have won this round! ${capitalizeFirstLetter(humanChoice)} beats ${capitalizeFirstLetter(computerChoice)}.`;
-    humanScore++;
-  } else if (rules[humanChoice].losesAgainst === computerChoice) {
-    msg = `You have lost this round! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(humanChoice)}.`;
-    computerScore++;
-  }
+  uiCheckbox.addEventListener("change", (e) => {
+    resetGame();
+    uiEnabled = e.target.checked;
+  });
 
-  console.log(msg);
-  alert(msg);
-}
+  playBtn.addEventListener("click", (e) => {
+    resetGame();
+    game.currentRound = 1;
+    console.log(`Round ${game.currentRound}`);
 
-function playGame() {
-  const numberOfRounds = 5;
-  let round = 1;
-  let msg;
+    if (!uiEnabled) {
+      while (!game.result) {
+        const humanChoice = getHumanChoice();
 
-  // Remove listeners on UI change
-  uiCheckbox.addEventListener(
-    "change",
-    (e) => {
-      eventListenerHelper("remove");
-    },
-    { once: true },
-  );
-
-  // Remove listeners on game reset
-  playBtn.addEventListener(
-    "click",
-    (e) => {
-      eventListenerHelper("remove");
-    },
-    { once: true },
-  );
-
-  if (!uiEnabled) {
-    while (round < numberOfRounds) {
-      try {
-        // Log beginning of each round
-        console.log(`Round ${round}`);
-
-        playRound(getHumanChoice(), getComputerChoice());
-        round++;
-      } catch (err) {
-        if (err.value === null) {
-          // End game if prompt window is closed
-          return console.log("Game ended.");
+        if (!humanChoice) {
+          resetGame();
+          return;
+        } else {
+          playRound(humanChoice, getComputerChoice());
         }
-
-        console.log(err);
       }
     }
+  });
 
-    isGameOver();
-  } else {
-    // Log the round on game start
-    console.log(`Round ${round}`);
+  rockBtn.addEventListener("click", (e) => {
+    if (uiEnabled) playRound(getHumanChoice("rock"), getComputerChoice());
+  });
 
-    // Initialize listeners on game start
-    eventListenerHelper("add");
+  paperBtn.addEventListener("click", (e) => {
+    if (uiEnabled) playRound(getHumanChoice("paper"), getComputerChoice());
+  });
+
+  scissorsBtn.addEventListener("click", (e) => {
+    if (uiEnabled) playRound(getHumanChoice("scissors"), getComputerChoice());
+  });
+
+  function resetGame() {
+    if (game.currentRound) {
+      console.log("Game has ended.");
+    }
+
+    game.humanScore = 1;
+    game.computerScore = 1;
+    game.currentRound = null;
+    game.result = null;
   }
 
-  function eventListenerHelper(state) {
-    if (state === "add") {
-      rockBtn.addEventListener("click", rockEventHandler);
-      paperBtn.addEventListener("click", paperEventHandler);
-      scissorsBtn.addEventListener("click", scissorsEventHandler);
-    } else if (state === "remove") {
-      rockBtn.removeEventListener("click", rockEventHandler);
-      paperBtn.removeEventListener("click", paperEventHandler);
-      scissorsBtn.removeEventListener("click", scissorsEventHandler);
+  function getComputerChoice() {
+    // Get random number between 1-3
+    const choice = Math.floor(Math.random() * 3) + 1;
+
+    switch (choice) {
+      case 1:
+        return "rock";
+      case 2:
+        return "paper";
+      case 3:
+        return "scissors";
     }
   }
 
-  function rockEventHandler() {
-    playRoundWrapper("rock");
-  }
+  function getHumanChoice(choice = "") {
+    const choiceIsValid = () => {
+      if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+        console.error(
+          `Invalid choice: "${choice}". Must be rock, paper, or scissors.`,
+        );
 
-  function paperEventHandler() {
-    playRoundWrapper("paper");
-  }
-
-  function scissorsEventHandler() {
-    playRoundWrapper("scissors");
-  }
-
-  function playRoundWrapper(choice) {
-    playRound(choice, getComputerChoice());
-
-    if (!isGameOver()) {
-      round++;
-      console.log(`Round ${round}`);
-    }
-  }
-
-  function isGameOver() {
-    // Declare game winner
-    if (round === numberOfRounds) {
-      if (humanScore === computerScore) {
-        msg = "Game over. It was a draw!";
-      } else if (humanScore > computerScore) {
-        msg = "Game over. You are the winner!";
-      } else {
-        msg = "Game over. You are the loser!";
+        return false;
       }
-
-      console.log(msg);
-      alert(msg);
 
       return true;
+    };
+
+    // If UI is disabled, keep prompting until valid or cancelled
+    if (!uiEnabled) {
+      while (true) {
+        choice = window.prompt(`Choose "rock", "paper" or "scissors"!`);
+
+        // Return null if user cancels prompt so game can be exited
+        if (choice === null) {
+          console.error("Prompt cancelled by user.");
+          return null;
+        }
+
+        choice = choice.toLowerCase();
+
+        // Return choice if valid, else re-prompt
+        if (choiceIsValid()) {
+          return choice;
+        }
+      }
+    } else {
+      choice = choice.toLowerCase();
+
+      // If UI is enabled, return choice if valid, else return null
+      if (choiceIsValid()) {
+        return choice;
+      } else {
+        return null;
+      }
+    }
+  }
+
+  function playRound(humanChoice, computerChoice) {
+    // Determine if a game has been initiated or finished
+    if (!game.currentRound || game.result) {
+      return console.error("No game in progress.");
     }
 
-    return false;
+    const capitalizeFirstLetter = (string) => {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    const isGameOver = () => {
+      let msg;
+
+      // Declare game winner
+      if (game.currentRound === game.numberOfRounds) {
+        if (game.humanScore === game.computerScore) {
+          msg = "Game over. It was a draw!";
+          game.result = "tie";
+        } else if (game.humanScore > game.computerScore) {
+          msg = "Game over. You are the winner!";
+          game.result = "win";
+        } else {
+          msg = "Game over. You are the loser!";
+          game.result = "loss";
+        }
+
+        console.log(msg);
+        alert(msg);
+
+        return true;
+      }
+
+      return false;
+    };
+
+    const rules = {
+      rock: { winsAgainst: "scissors", losesAgainst: "paper" },
+      paper: { winsAgainst: "rock", losesAgainst: "scissors" },
+      scissors: { winsAgainst: "paper", losesAgainst: "rock" },
+    };
+
+    let msg;
+
+    // Determine winner and increase scores
+    if (humanChoice === computerChoice) {
+      msg = "It's a draw!";
+    } else if (rules[humanChoice].winsAgainst === computerChoice) {
+      msg = `You have won this round! ${capitalizeFirstLetter(humanChoice)} beats ${capitalizeFirstLetter(computerChoice)}.`;
+      game.humanScore++;
+    } else if (rules[humanChoice].losesAgainst === computerChoice) {
+      msg = `You have lost this round! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(humanChoice)}.`;
+      game.computerScore++;
+    }
+
+    console.log(msg);
+    alert(msg);
+
+    if (!isGameOver()) {
+      game.currentRound++;
+      console.log(`Round: ${game.currentRound}`);
+    }
   }
 }
